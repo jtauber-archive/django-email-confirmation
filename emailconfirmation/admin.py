d1894d37589428eab00f0de521e49902a3dfc367
James Tauber <jtauber@jtauber.com>
2014-08-01 00:44:56 -0400

    removed everything but README explaining move

--- emailconfirmation/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from emailconfirmation.models import EmailAddress, EmailConfirmation
-
-admin.site.register(EmailAddress)
-admin.site.register(EmailConfirmation)
--- emailconfirmation/signals.py
+++
@@ -1,4 +0,0 @@
-from django.dispatch import Signal
-
-
-email_confirmed = Signal(providing_args=["email_address"])
